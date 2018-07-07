591cce619049c0298343c34cf04e1f458c8f9532
wei li <liweiliwei0103@outlook.com>
2018-07-07 09:53:45 +0800

    暂时放一放

--- SpiderHAP/Form1.cs
+++ SpiderHAP/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,17 @@ namespace SpiderHAP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //窗体初始化，在这里做测试
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string url = "http://t66y.com/htm_data/7/1807/3201612.html";
+
+            HtmlAgilityPack.HtmlWeb hw = new HtmlAgilityPack.HtmlWeb();
+
+            HtmlAgilityPack.HtmlDocument doccc = hw.Load(url);//是你需要解析的url
+
         }
     }
 }
